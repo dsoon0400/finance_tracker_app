59deee2b2fb622590aa1a5fc90bee03c34985aa1
dsoon0400 <115666287+dsoon0400@users.noreply.github.com>
2024-11-08 14:11:42 -0500

    Update expense_screen.dart

--- lib/screens/expense_screen.dart
+++ lib/screens/expense_screen.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 import '../widgets/expense_list.dart';
 import '../widgets/add_expense.dart';
+import '../models/expense.dart';
 
-class ExpenseScreen extends StatelessWidget {
+class ExpenseScreen extends StatefulWidget {
+  @override
+  _ExpenseScreenState createState() => _ExpenseScreenState();
+}
+
+class _ExpenseScreenState extends State<ExpenseScreen> {
+  final List<Expense> _expenses = [];
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _expenses.add(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Expenses')),
       body: Column(
         children: [
-          AddExpense(),
-          Expanded(child: ExpenseList()),
+          AddExpense(onAddExpense: _addExpense),
+          Expanded(child: ExpenseList(expenses: _expenses)),
         ],
       ),
     );
