c5813d4c667849ab1a998004210fb04a8b9e8d85
dsoon0400 <115666287+dsoon0400@users.noreply.github.com>
2024-11-08 14:12:02 -0500

    Update expense_list.dart

--- lib/widgets/expense_list.dart
+++ lib/widgets/expense_list.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import '../models/expense.dart';
 
 class ExpenseList extends StatelessWidget {
+  final List<Expense> expenses;
+
+  ExpenseList({required this.expenses});
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [Text('Expense 1'), Text('Expense 2')], // Placeholder for list items
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) {
+        final expense = expenses[index];
+        return ListTile(
+          title: Text(expense.title),
+          subtitle: Text('\$${expense.amount} on ${expense.date.toLocal()}'),
+        );
+      },
     );
   }
 }
