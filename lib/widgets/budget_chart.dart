06f493ce69525b624130fc88303334351e285d99
dsoon0400 <115666287+dsoon0400@users.noreply.github.com>
2024-11-08 14:12:25 -0500

    Update budget_chart.dart

--- lib/widgets/budget_chart.dart
+++ lib/widgets/budget_chart.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class BudgetChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  BudgetChart(this.seriesList, {this.animate = false});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Budget Chart Placeholder'),
+      height: 300,
+      child: charts.BarChart(
+        seriesList,
+        animate: animate,
+      ),
     );
   }
 }
